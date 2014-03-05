b909424e39f5cbf164bd87f820aaf2cae0e448b9
Zack Bloom <zackbloom@gmail.com>
2014-03-05 13:38:43 -0500

    Fix whitespace
    Just cause the computer doesn't care doesn't mean we shouldn't.

--- hsb_suggest.js
+++ hsb_suggest.js
@@ -4,37 +4,41 @@ onscroll = function() {
     var scrollSpot = document.documentElement.scrollTop || document.body.scrollTop;
     if (w > 300) {
         var scrollPerc = document.body.scrollHeight*.50;
-    }
-    else {
+    } else {
         var scrollPerc = document.body.scrollHeight*.85;
     }
+    
     if (scrollSpot > scrollPerc) {
         if (!appended){
             var popBox = document.getElementById("suggest_box");
             var x = 0;
+            
             function slowLoop1(){
-                if (x<10){
-                popBox.style.opacity=x*0.1;      
-                x++;
-                setTimeout(slowLoop1, 50);
+                if (x < 10){
+                    popBox.style.opacity = x * 0.1;      
+                    x++;
+                    setTimeout(slowLoop1, 50);
                 }
             }
             slowLoop1();
+            
             appended = true;
         }
     } else {
         if (appended) {
             var popBox = document.getElementById("suggest_box");
-            var x = 10
+            var x = 10;
+            
             function slowLoop2(){
-                if (x>-1){
-                popBox.style.opacity=x*0.1;      
-                x--;
-                setTimeout(slowLoop2, 50);
+                if (x > -1){
+                    popBox.style.opacity = x * 0.1;      
+                    x--;
+                    setTimeout(slowLoop2, 50);
                 }
             }
             slowLoop2();
-            appended=false;
+            
+            appended = false;
         }
     }
 };
@@ -42,15 +46,16 @@ onscroll = function() {
 
 var closeMe = document.getElementsByTagName("button");
 closeMe[0].onclick = function () {
-            var popBox = document.getElementById("suggest_box");
-            var x = 10
-            function slowLoop2(){
-                if (x>-1){
-                popBox.style.opacity=x*0.1;      
-                x--;
-                setTimeout(slowLoop2, 50);
-                }
-            }
-            slowLoop2();
-            appended=true;
-        };
+    var popBox = document.getElementById("suggest_box");
+    var x = 10
+    
+    function slowLoop2(){
+        if (x > -1){
+            popBox.style.opacity=x*0.1;      
+            x--;
+            setTimeout(slowLoop2, 50);
+        }
+    }
+    slowLoop2();
+    appended = true;
+};
